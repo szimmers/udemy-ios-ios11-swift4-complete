d1c4f9c00a34452769e1bb7cb5c52c661a061afb
Steve Zimmers <steve@soccer-1.com>
2018-02-27 16:11:30 -0500

    add ability to play sounds

--- Xylophone-iOS11-master/Xylophone/ VIewController.swift
+++ Xylophone-iOS11-master/Xylophone/ VIewController.swift
@@ -7,23 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
-    
-
+	
+	var player:AVAudioPlayer?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
+	func playSound(_ noteNum:Int) {
+		let noteName:String = "note\(noteNum)"
+		let url = Bundle.main.url(forResource: noteName, withExtension: "wav")!
+		
+		do {
+			player = try AVAudioPlayer(contentsOf: url)
+			guard let player = player else { return }
+			
+			player.prepareToPlay()
+			player.play()
+		} catch let error as NSError {
+			print(error.description)
+		}
+	}
+	
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+		playSound(sender.tag)
     }
-    
-  
 
 }
 
